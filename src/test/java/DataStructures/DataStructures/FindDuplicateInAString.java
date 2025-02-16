2a5f4c2dafa54d19db781120883ba54ac6a17965
muralisankar321 <muralisankar321@gmail.com>
2025-02-16 16:40:59 +0530

    updating find duplicate character in a string

--- src/test/java/DataStructures/DataStructures/FindDuplicateInAString.java
+++ src/test/java/DataStructures/DataStructures/FindDuplicateInAString.java
@@ -1,33 +1,45 @@
 package DataStructures.DataStructures;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class FindDuplicateInAString {
-	
+
 	public static void main(String[] args) {
-		
-		HashMap<String,Integer> map = new HashMap<String,Integer>();
-		
+
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+
 		String s = "aaa bbb vvv ccc rrr ddd aaa vvv";
 		String[] str = s.split(" ");
-		
-		for(int i=0;i<str.length;i++) {
-			
-			
-			if(!map.containsKey(str[i])) {
-				
+		StringBuffer sb = new StringBuffer();
+		StringBuffer sb1 = new StringBuffer();
+		for (int i = 0; i < str.length; i++) {
+
+			if (!map.containsKey(str[i])) {
+
 				map.put(str[i], 1);
+			} else {
+				map.put(str[i], map.get(str[i]) + 1);
 			}
-			else {
-				map.put(str[i], map.get(str[i])+1);
+
+		}
+
+		for (Map.Entry<String, Integer> m : map.entrySet()) {
+
+			System.out.println(m.getKey() + " " + m.getValue());
+
+			if (m.getValue() > 1) {
+
+				sb.append(m.getKey() + " ");
 			}
+
+			sb1.append(m.getKey() + " ");
+
 		}
-		
-		for(Map.Entry<String, Integer> m: map.entrySet()) {
-			
-			System.out.println(m.getKey()+" "+m.getValue());
-		}
-		
+
+		System.out.println("duplicate strings are :" + sb.toString());
+		System.out.println("Strings without duplicates are :" + sb1.toString());
+
 	}
 
 }
