035c7a9a1a7382aaabd8f064b285d32d03f9095d
muralisankar321 <muralisankar321@gmail.com>
2025-02-17 22:34:48 +0530

    Substring Practice

---
+++ src/test/java/DataStructures/DataStructures/StringSubStringPractice.java
@@ -0,0 +1,17 @@
+package DataStructures.DataStructures;
+
+public class StringSubStringPractice {
+	
+	
+	public static void main(String args[]) {
+		
+		String str = "Java World";
+		
+		String strSubcomma = str.substring(0, 4);
+		System.out.println(strSubcomma);
+		
+		String strSubIndex = str.substring(2);
+		System.out.println(strSubIndex);
+	}
+
+}
